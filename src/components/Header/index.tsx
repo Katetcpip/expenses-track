f1f5e207d99ef6f02f6644e87dff259e7ffda577
Katetcpip <katetcpip@yabdex.ru>
2023-03-09 21:14:58 +0300

    css fixes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,8 +3,8 @@ import 'components/Header/styles.css';
 function Header(){
     return(
         <div className='flex flex-row w-full justify-center items-center'>
-        <img alt='' src='http://www.portaleterzosettore.it/wp-content/uploads/2015/05/contabilità.png' className='lg:h-4/12 lg:w-4/12 h-4/12 w-1/2 md:-mt-18 md:-ml-24 -mt-14 -ml-16'></img>
-        <p className="text-zinc-700 font-bold md:text-5xl text-3xl text-start underline w-full pl-4">Expenses</p> 
+        <img alt='' src='http://www.portaleterzosettore.it/wp-content/uploads/2015/05/contabilità.png' className='lg:h-4/12 lg:w-4/12 h-4/12 w-1/2 md:-mt-18 md:-ml-24 -mt-14 -ml-14'></img>
+        <p className="text-zinc-700 font-bold md:text-5xl text-3xl text-start underline w-full lg:pl-4 pl-1">Expenses</p> 
     </div>
     );
 }
